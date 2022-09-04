b3780b701655c9afd78dcf9dbe9c9c525dbba3e0
cbrobak <cbrobak79@gmail.com>
2022-09-04 01:28:20 -0400

    Checkpoint

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const peopleButton = document.getElementsByTagName("button"),
     nameHeading = document.getElementById("nameHeading"),
     bioInfo = document.getElementById("bioInfo"),
-    li = document.createElement("li")
-const list = document.createElement('ul')
+    list = document.createElement('ul'),
+    films = "https://swapi.dev/api/films/"
+
+
+String.prototype.capper = function () {
+    let str = this;
+    return str[0].toUpperCase() + str.slice(1)
+};
+
+function sorting(a, b) {
+    return a - b
+}
 
 function myFunc(id) {
     switch (id) {
@@ -10,17 +20,92 @@ function myFunc(id) {
             getPeopleData(id)
             break;
         case 'films':
-            getFilmData(id)
+            getFilmList(films)
         default:
             break;
     }
 }
 
-function getMovieData(title) {
-    console.log("title", title)
+function getFilmList(url) {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (!data) {
+                alert("We are having technical difficulties, please try again later.")
+            }
+            for (const key in data) {
+                if (Object.hasOwnProperty.call(data, key)) {
+                    let element = data[key];
+                    if (Array.isArray(element)) {
+                        element.forEach(elem => {
+                            title = document.createElement('button')
+                            title.innerHTML = `${elem.title}`
+                            title.classList.add("col-4", "offset-4")
+                            $(bioInfo).append(title)
+                            title.onclick = () => {
+                                getMovieData(elem.url)
+                            }
+                        })
+                    }
+                }
+            }
+        })
+}
+
+function getMovieData(url) {
+    list.replaceChildren()
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (!data) {
+                alert("We are having technical difficulties, please try again later.")
+            }
+            for (const key in data) {
+
+                switch (key) {
+                    case "title":
+                        $(bioInfo).append(`
+                        <div class=" col-12"><h1 class="text-warning">${key.capper()}: ${data[key]}</h1></div>`)
+                        break;
+                    case "director":
+                        $(bioInfo).append(`
+                        <div class=" col-12"><h1 class="text-warning">${key.capper()}: ${data[key]}</h1></div>`)
+                        break;
+                    case "producer":
+                        $(bioInfo).append(`
+                            <div class=" col-12"><h1 class="text-warning">${key.capper()}: ${data[key]}</h1></div>`)
+                        break;
+                    case "characters":
+                        $(bioInfo).append(`<h1 class="text-warning">Characters: `)
+                        if (Array.isArray(data.characters)) {
+                            data.characters.forEach(element => {
+                                fetch(element)
+                                    .then((response) => response.json())
+                                    .then((data) => {
+                                        for (const key in data) {
+                                            if (key === "name") {
+                                                button = document.createElement('button')
+                                                button.innerHTML = `${data.name}`
+                                                $(list).append(button)
+                                                $(bioInfo).append(list)
+                                                button.onclick = () => {
+                                                    getBioData(data.url.slice(22))
+                                                }
+                                            }
+
+                                        }
+                                    })
+                            })
+                        }
+                        break;
+                }
+            }
+        })
+    bioInfo.replaceChildren()
 }
 
 function getBioData(name) {
+    bioInfo.replaceChildren()
     fetch(`https://swapi.dev/api/${name}`)
         .then((response) => response.json())
         .then((data) => {
@@ -46,23 +131,27 @@ function getBioData(name) {
                         break;
                     case 'films':
                         const films = data[key]
-                        $(list).append(`<h3 class="text-warning">Films</h3>`)
+                        $(bioInfo).append(`<h3 class="text-warning">Films</h3>`)
                         films.forEach(element => {
-                            fetch(`https://swapi.dev/api/${element.slice(22)}`)
+                            fetch(element)
                                 .then((response) => response.json())
                                 .then((data) => {
-                                    const title = data.url.slice(22)
-                                    console.log("title", title)
+                                    const button = document.createElement("button")
+                                    button.classList.add("d-flex", "flex-column", "col-4", "offset-4")
+                                    button.innerHTML = `${data.title}`
+                                    $(list).append(button)
                                     $(bioInfo).append(list)
-                                    $(list).append(`<li><h3 class="text-primary">Title: <a href="#" class="text-danger" onclick="getMovieData(${title})">
-                                        ${data.title}</a></h3></li`)
+                                    button.onclick = () => {
+                                        getMovieData(data.url)
+                                    }
                                 })
                         })
-                        films.sort();
+                        films.sort(sorting);
                     default:
                         break;
                 }
             }
+            list.replaceChildren()
             nameHeading.replaceChildren()
         })
 }
@@ -81,7 +170,7 @@ function getPeopleData(searchTerm) {
                         element.forEach(elem => {
                             names = document.createElement('button')
                             names.innerHTML = `${elem.name}`
-                            names.classList.add('bg-dark', "mt-1", "col-3", "text-danger")
+                            names.classList.add("col-4", "offset-4")
                             $("#nameHeading").append(names)
                             names.onclick = () => {
                                 getBioData(elem.url.slice(22))
